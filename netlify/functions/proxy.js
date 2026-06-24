fa734556b4a60724415c147d778d32d52f8f6e56
shayara magar <shyaramgr5544@gmail.com>
2026-06-24 14:33:26 +0545

    fix proxy

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,6 +1,8 @@
+const https = require('https');
+
 exports.handler = async function(event) {
-  const SCRIPT_URL = "https://https://script.google.com/macros/s/AKfycbwJfSSbp_LEkCA2lbA0_nDctPNJGGZZ4x6TdfcBrHu-Le86mIle4BH3OrhZYXl2zRM/exec";
-  
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyv-QLqPnb8PJoXHUvVQbEFiRCtQOHp5yqILrIfDmO51Vi-V3DtzyyJwaUfB6i9QDI/exec";
+
   if (event.httpMethod === 'OPTIONS') {
     return {
       statusCode: 200,
@@ -13,30 +15,39 @@ exports.handler = async function(event) {
     };
   }
 
-  try {
-    const url = event.queryStringParameters && event.queryStringParameters.action 
-      ? SCRIPT_URL + '?action=' + event.queryStringParameters.action
-      : SCRIPT_URL;
-    
-    const options = { method: event.httpMethod };
-    if (event.body) options.body = event.body;
-    
-    const response = await fetch(url, options);
-    const data = await response.text();
-    
-    return {
-      statusCode: 200,
-      headers: { 
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      body: data
+  const action = event.queryStringParameters && event.queryStringParameters.action;
+  const targetUrl = action ? SCRIPT_URL + '?action=' + action : SCRIPT_URL;
+
+  return new Promise((resolve) => {
+    const urlObj = new URL(targetUrl);
+    const options = {
+      hostname: urlObj.hostname,
+      path: urlObj.pathname + urlObj.search,
+      method: event.httpMethod,
+      headers: {}
     };
-  } catch(err) {
-    return {
-      statusCode: 500,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ success: false, error: err.toString() })
-    };
-  }
+
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+          body: data
+        });
+      });
+    });
+
+    req.on('error', (err) => {
+      resolve({
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({ success: false, error: err.toString() })
+      });
+    });
+
+    if (event.body) req.write(event.body);
+    req.end();
+  });
 };
