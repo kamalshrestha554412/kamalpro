8c3060a4a7440a167a68becde7616366e348deeb
shayara magar <shyaramgr5544@gmail.com>
2026-06-24 14:40:33 +0545

    fix redirect

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -18,36 +18,47 @@ exports.handler = async function(event) {
   const action = event.queryStringParameters && event.queryStringParameters.action;
   const targetUrl = action ? SCRIPT_URL + '?action=' + action : SCRIPT_URL;
 
-  return new Promise((resolve) => {
-    const urlObj = new URL(targetUrl);
-    const options = {
-      hostname: urlObj.hostname,
-      path: urlObj.pathname + urlObj.search,
-      method: event.httpMethod,
-      headers: {}
+  function fetchWithRedirect(url, options, body, redirectCount) {
+    redirectCount = redirectCount || 0;
+    if (redirectCount > 5) return Promise.reject(new Error('Too many redirects'));
+
+    return new Promise((resolve, reject) => {
+      const urlObj = new URL(url);
+      const reqOptions = {
+        hostname: urlObj.hostname,
+        path: urlObj.pathname + urlObj.search,
+        method: options.method || 'GET',
+        headers: { 'Content-Type': 'text/plain' }
+      };
+
+      const req = https.request(reqOptions, (res) => {
+        if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+          resolve(fetchWithRedirect(res.headers.location, { method: 'GET' }, null, redirectCount + 1));
+          return;
+        }
+        let data = '';
+        res.on('data', chunk => data += chunk);
+        res.on('end', () => resolve(data));
+      });
+
+      req.on('error', reject);
+      if (body) req.write(body);
+      req.end();
+    });
+  }
+
+  try {
+    const data = await fetchWithRedirect(targetUrl, { method: event.httpMethod }, event.body);
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+      body: typeof data === 'string' ? data : JSON.stringify(data)
     };
-
-    const req = https.request(options, (res) => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        resolve({
-          statusCode: 200,
-          headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-          body: data
-        });
-      });
-    });
-
-    req.on('error', (err) => {
-      resolve({
-        statusCode: 500,
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        body: JSON.stringify({ success: false, error: err.toString() })
-      });
-    });
-
-    if (event.body) req.write(event.body);
-    req.end();
-  });
+  } catch(err) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ success: false, error: err.toString() })
+    };
+  }
 };
